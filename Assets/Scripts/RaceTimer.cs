8709a3cbb472e4c1332fac34402209dfcaf5a89e
Rivera8840 <andresrivera888@gmail.com>
2025-03-09 01:40:11 -0800

    Added a timer UI

---
+++ Assets/Scripts/RaceTimer.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class RaceTimer : MonoBehaviour
+{
+    public TMP_Text timerText;  // Assign the TimerText UI object in the inspector
+    private float timer = 0.0f;
+    private bool isRunning = false;
+
+    void Start()
+    {
+        isRunning = true; // Start the timer when the scene loads
+    }
+
+    void Update()
+    {
+        if (isRunning)
+        {
+            timer += Time.deltaTime;
+            UpdateTimerDisplay();
+        }
+    }
+
+    void UpdateTimerDisplay()
+    {
+        int minutes = Mathf.FloorToInt(timer / 60);
+        int seconds = Mathf.FloorToInt(timer % 60);
+        int milliseconds = Mathf.FloorToInt((timer * 100) % 100);
+
+        timerText.text = $"Timer: {minutes:00}:{seconds:00}:{milliseconds:00}";
+    }
+
+    public void StartTimer()
+    {
+        isRunning = true;
+    }
+
+    public void StopTimer()
+    {
+        isRunning = false;
+    }
+
+    public void ResetTimer()
+    {
+        timer = 0.0f;
+        UpdateTimerDisplay();
+    }
+}
